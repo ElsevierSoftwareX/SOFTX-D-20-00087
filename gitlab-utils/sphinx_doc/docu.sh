6d22bd432c28e84f20fdac3dff0d59a33c5e3049
ss391347 <sebastian.schwarz1@rwth-aachen.de>
2020-11-24 13:21:53 +0100

    Modified file docu.sh to also generate the documentation if the script is executed from another path.

--- gitlab-utils/sphinx_doc/docu.sh
+++ gitlab-utils/sphinx_doc/docu.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-rm pycity_scheduling.*.rst
-sphinx-apidoc -F -H "pycity_scheduling" -o "." "../../src/pycity_scheduling"
-make clean
-make html
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DOCU_MAKE_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+rm -rf "${DOCU_MAKE_DIR}/pycity_scheduling.*.rst"
+sphinx-apidoc -F -H "pycity_scheduling" -o "${DOCU_MAKE_DIR}" "${DOCU_MAKE_DIR}/../../src/pycity_scheduling"
+make -C "${DOCU_MAKE_DIR}" clean
+make -C "${DOCU_MAKE_DIR}" html
