2275fc93147d04da0ad67022a2a03883762a0325
ss391347 <sebastian.schwarz1@rwth-aachen.de>
2021-06-18 08:08:19 +0200

    Changed pycity_scheduling default optimization solver to gurobi.

--- src/pycity_scheduling/solvers.py
+++ src/pycity_scheduling/solvers.py
@@ -24,13 +24,16 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 
 # Specify different third-party mathematical programming solvers and solver options:
 
-SCIP_SOLVER = "scip"
-SCIP_SOLVER_OPTIONS = {'solve': {'options': {}}}
-
 BONMIN_SOLVER = "bonmin"
 BONMIN_SOLVER_OPTIONS = {'solve': {'options': {'bonmin.algorithm': 'b-hyb',
                                                'bonmin.allowable_gap': 1e-10,
                                                'bonmin.allowable_fraction_gap': 1e-4}}}
+
+CPLEX_SOLVER = "cplex"
+CPLEX_SOLVER_OPTIONS = {'solve': {'options': {}}}
+
+GLPK_SOLVER = "glpk"
+GLPK_SOLVER_OPTIONS = {'solve': {'options': {}}}
 
 GUROBI_DIRECT_SOLVER = "gurobi_direct"
 GUROBI_DIRECT_SOLVER_OPTIONS = {'solve': {'options': {'OutputFlag': 0,
@@ -44,10 +47,10 @@ GUROBI_PERSISTENT_SOLVER_OPTIONS = {'solve': {'options': {'OutputFlag': 0,
                                                           'Logfile': "",
                                                           "Method": 1}}}
 
-CPLEX_SOLVER = "cplex"
-CPLEX_SOLVER_OPTIONS = {'solve': {'options': {}}}
+SCIP_SOLVER = "scip"
+SCIP_SOLVER_OPTIONS = {'solve': {'options': {}}}
 
 
 # Set the default mathematical programming solver to be used by the pycity_scheduling framework:
-DEFAULT_SOLVER = SCIP_SOLVER
-DEFAULT_SOLVER_OPTIONS = SCIP_SOLVER_OPTIONS
+DEFAULT_SOLVER = GUROBI_DIRECT_SOLVER
+DEFAULT_SOLVER_OPTIONS = GUROBI_DIRECT_SOLVER_OPTIONS
